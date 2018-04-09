4714e82c7d82595f0f784217c1ad9635d90c2d80
hassangamal <gamalhassan807@yahoo.com>
2018-04-09 19:13:07 +0200

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,6 +5,7 @@ namespace CatchAbook\Http\Controllers;
 use Illuminate\Http\Request;
 use CatchAbook\User;
 use CatchAbook\book;
+use CatchAbook\comment;
 use Auth;
 use Illuminate\Support\Collection;
 
@@ -29,24 +30,47 @@ class HomeController extends Controller
     {
         $user = Auth::user();
         $books = $user->book;
-        return view('home')->with('books', $books);
+        $comments = $user->comment;
+        return view('home')->with('books', $books)->with('user',$user)->with('comments',$comments);
     }
-
-    public function mybrow()
+    public function myborrow()
     {
         $user = Auth::user();
         $books = $user->book->where('borrow',1);
-        return view('home')->with('books', $books);
+        $comments = $user->comment;
+        return view('home')->with('books', $books)->with('user',$user)->with('comments',$comments);
     }
-    public function brow()
+    public function borrow()
     {
         $user = Auth::user();
         $books = $user->book->where('borrow',0);
-        return view('home')->with('books', $books);
-
+        $comments = $user->comment;
+        return view('home')->with('books', $books)->with('user',$user)->with('comments',$comments);
     }
     public function add()
     {
         return view('add');
+    }
+    public function delete($id)
+    {
+        $user_book=book::find($id);
+        $user=User::find($user_book->user_id);
+        $user->borrows-=1;
+        $user->save();
+        $user_book->delete();
+        return redirect('home');
+    }
+    public function addcomment(Request $request ,$id)
+    {
+        $user_comment=new comment;
+        $user_comment->comment=$request->comment;
+        $user_comment->user_id=$id;
+        $user_comment->save();
+        return redirect('home');
+    }
+    public function edit($id)
+    {
+        $user_book=book::find($id);
+        return view('edit')->with('user_book',$user_book);
     }
 }
