5c3ed19e54020d48cf9ab4b97ca5e21f9b7127d4
hassangamal <gamalhassan807@yahoo.com>
2018-04-09 19:14:57 +0200

    Update 2014_10_12_000000_create_users_table.php

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,8 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
+            $table->integer('myborrows')->default('0');;
+            $table->integer('borrows')->default('0');;
             $table->rememberToken();
             $table->timestamps();
         });
